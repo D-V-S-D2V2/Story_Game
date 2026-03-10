788b646319f834c30735051b8853aa693cbbbd1c
Darsh Pratap Singh <dpsingh1@my.waketech.edu>
2026-03-10 14:08:44 -0400

    im done

---
+++ script.js
@@ -0,0 +1,6 @@
+let story_image = document.getElementById("story_image");
+let title = document.getElementById("title");
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let 
