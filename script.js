83a90b8da1b661eb1221b99a0b8bfd233c2d6423
Darsh Pratap Singh <dpsingh1@my.waketech.edu>
2026-03-11 11:44:35 -0400

    weird stuff happening

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ let title = document.getElementById("title");
 let p1 = document.getElementById("p1");
 let p2 = document.getElementById("p2");
 let p3 = document.getElementById("p3");
-let 
+
+
+option1.addEventListener('click', function() {
+    option1.classList.add('selected');
+    option2.classList.remove('selected');
+    option3.classList.remove('selected');
+});
+
+option2.addEventListener('click', function() {
+    option2.classList.add('selected');
+    option1.classList.remove('selected');
+    option3.classList.remove('selected');
+});
+
+option3.addEventListener('click', function() {
+    option3.classList.add('selected');
+    option2.classList.remove('selected');
+    option1.classList.remove('selected');
+});
